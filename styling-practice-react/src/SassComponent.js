3d2edaa544dcdde7e5a3254297cdff0f1d062c7b
(seonwoo kim) <(sw204011@gmail.com)>
2023-03-18 18:18:18 +0900

    Commit before yarn eject

---
+++ styling-practice-react/src/SassComponent.js
@@ -0,0 +1,13 @@
+import "./SassComponent.scss";
+
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box blue" />
+      <button className="box green">누름</button>
+      <div className="box normal" />
+    </div>
+  );
+};
+
+export default SassComponent;
