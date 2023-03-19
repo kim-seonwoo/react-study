ed71a282275fed82ee1c041f94afd968d0094382
(seonwoo kim) <(sw204011@gmail.com)>
2023-03-19 16:19:15 +0900

    '0319re'

--- styling-practice-react/StyledComponents.js
+++ styling-practice-react/StyledComponents.js
@@ -1,2 +1,23 @@
+import styled, { css } from "styled-components";
 
+const box = styled.div`
+  bckground: ${(props) => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
+`;
 
+const button = styled.button`
+  background: white;
+  color: black;
+  border-radius: 4px;
+`;
+
+const MyInput = styled("input")`
+  background: "yellow";
+`;
+// 사용할 태그의 이름이 유동적이거나 컴포넌트 자체를 스타일링 해주고 싶을때,
+// 태그의 타입을 styled함수의 인자로 전달
+
+const MyLink = styled(Link)`
+  background: "blue";
+`;
