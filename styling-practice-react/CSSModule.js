cfd7a651f4b677dec983deca8ca8b650d19eed9d
(seonwoo kim) <(sw204011@gmail.com)>
2023-03-19 16:21:05 +0900

    '0319-third'

---
+++ styling-practice-react/CSSModule.js
@@ -0,0 +1,12 @@
+import classNames from "classnames";
+import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles); //미리 styles에서 class를 받아오도록 설정하고
+
+const CSSModule = () => {
+  return (
+    <div className={cx("wrapper", "inverted")}>
+      안녕하세요.<span className="something">CSS</span>
+    </div>
+  );
+};
